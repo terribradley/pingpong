ac0f3708bcde5563e471538c1b7c0dee78011b48
Terri Bradley <TerriBradley@terri-bradleys-macbook-2.local>
2016-06-10 08:59:52 -0700

    Splices numbers divisible by 15 and 5.

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,40 @@
 var pingPongFunction = function(userInput) {
   var numbers = []
 
-  for (index = 0; index < userInput; index ++) {
-    numbers.push(index)
-      debugger;
-  };
+  for (index = 1; index <= userInput; index ++) {
+    numbers.push(index);
+  }
 
-  for (index = 0; index <= numbers.length; index ++)
-    if (index%15 === 0) {
-      result = numbers.splice(i,1).push("PingPong");
-     debugger;
-   } else if (index%5 === 0) {
-        result = numberList.push("Pong");
-    } else if (index %3 === 0) {
-      result = numberList.push("Ping");
-    } else {
-      result = numberList.push(i);
- console.log();
+  for (index = 0; index <= numbers.length; index ++) {
+    var divisiableNum = numbers.length;
+    for (i = divisiableNum; i > index; i--) {
+      if (numbers[i] %15 === 0) {
+      numbers.splice(i,1);
+    } else if (numbers[i] %5 === 0) {
+      numbers.splice (i,1);
+      debugger;
+    }
   }
 };
+};
+      // .push("PingPong")
+    //  debugger;
 
- };
+      // } else if (numbers[i] %3 === 0) {
+      // result = numberList.push("Ping");
+      // } else {
+      // result = numberList.push(i);
+ // console.log();
+
+
+
+ // };
 
 
 $(document).ready(function() {
   $(".ping-pong").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#user-input").val());
-    Numbers = pingPongFunction(input);
-});
+    numbers = pingPongFunction(input);
+  });
 });
